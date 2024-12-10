cb9048fe9353b69e5a2f47907eddd13afaca07ca
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-12-10 00:18:59 -0500

    Update Swift to 6.0.2

--- package/swift/swift.mk
+++ package/swift/swift.mk
@@ -1,5 +1,5 @@
 ### Apple's Swift Programming Language
-SWIFT_VERSION = 5.10
+SWIFT_VERSION = 6.0.2
 SWIFT_SITE = $(call github,apple,swift,swift-$(SWIFT_VERSION)-RELEASE)
 SWIFT_LICENSE = Apache-2.0
 SWIFT_LICENSE_FILES = LICENSE.txt
