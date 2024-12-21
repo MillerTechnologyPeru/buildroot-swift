3d6d5be39024f960f7b6fefbeefcc46f97505ba2
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-12-21 07:17:04 -0500

    Add build scripts

--- .devcontainer/build-scripts/build-docker.sh
+++ .devcontainer/build-scripts/build-docker.sh
@@ -9,13 +9,17 @@ DEFCONFIG="${DEFCONFIG:=imx6slevk_swift_defconfig}"
 DOCKER_FILE_ARCH=$SWIFT_BUILDROOT/.devcontainer/Dockerfile-$DEFCONFIG
 rm -rf $DOCKER_FILE_ARCH
 echo "FROM colemancda/buildroot-swift" >> $DOCKER_FILE_ARCH
+echo "ENV DEFCONFIG=${DEFCONFIG}" >> $DOCKER_FILE_ARCH
 echo "ENV SWIFT_NATIVE_TOOLS=/workspaces/swift/usr/bin" >> $DOCKER_FILE_ARCH
 echo "ENV SWIFT_LLVM_DIR=/workspaces/llvm" >> $DOCKER_FILE_ARCH
-echo "ENV BUILDROOT_DIR=/workspaces/buildroot" >> $DOCKER_FILE_ARCH
 echo "ENV SWIFT_BUILDROOT=/workspaces/buildroot-swift" >> $DOCKER_FILE_ARCH
 echo "ENV BUILDROOT_RELEASE=2024.02.9" >> $DOCKER_FILE_ARCH
-echo "ENV DEFCONFIG=${DEFCONFIG}" >> $DOCKER_FILE_ARCH
+echo "ENV BUILDROOT_DIR=/workspaces/buildroot" >> $DOCKER_FILE_ARCH
 echo "COPY . /workspaces/buildroot-swift/" >> $DOCKER_FILE_ARCH
+echo "COPY .devcontainer/build-scripts/configure.sh /tmp/build-scripts/" >> $DOCKER_FILE_ARCH
+echo "RUN /bin/bash /tmp/build-scripts/configure.sh" >> $DOCKER_FILE_ARCH
+echo "COPY .devcontainer/build-scripts/fetch-sources.sh /tmp/build-scripts/" >> $DOCKER_FILE_ARCH
+echo "RUN /bin/bash /tmp/build-scripts/fetch-sources.sh" >> $DOCKER_FILE_ARCH
 echo "COPY .devcontainer/build-scripts/build-base.sh /tmp/build-scripts/" >> $DOCKER_FILE_ARCH
 echo "RUN /bin/bash /tmp/build-scripts/build-base.sh" >> $DOCKER_FILE_ARCH
 
--- .devcontainer/build-scripts/fetch-sources.sh
+++ .devcontainer/build-scripts/fetch-sources.sh
@@ -7,7 +7,3 @@ BUILDROOT_DIR="${BUILDROOT_DIR:=/workspaces/buildroot}"
 # Build
 cd $BUILDROOT_DIR
 make source
-make libbsd-build
-make libxml2-build
-make openssl-build
-make libcurl-build
