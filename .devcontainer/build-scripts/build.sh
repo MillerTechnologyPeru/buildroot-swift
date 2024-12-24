13e25bce2b5dd27b9a626f983f5199db129d5e22
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-12-24 05:26:57 -0500

    Update Docker image build script

--- .devcontainer/build-scripts/build.sh
+++ .devcontainer/build-scripts/build.sh
@@ -5,8 +5,6 @@ set -e
 SWIFT_BUILDROOT="${SWIFT_BUILDROOT:=$(pwd)}"
 DEFCONFIG="${DEFCONFIG:=swift_arm64_defconfig}"
 BUILDROOT_DIR="${BUILDROOT_DIR:=/workspaces/buildroot}"
-SWIFT_NATIVE_TOOLS="${SWIFT_NATIVE_TOOLS:=/workspaces/swift/usr/bin}"
-SWIFT_LLVM_DIR="${SWIFT_LLVM_DIR:=/workspaces/llvm}"
 
 # Prepare environment
 $SWIFT_BUILDROOT/.devcontainer/build-scripts/configure.sh
--- .devcontainer/build-scripts/download-buildroot.sh
+++ .devcontainer/build-scripts/download-buildroot.sh
@@ -4,10 +4,8 @@ set -e
 BUILDROOT_RELEASE="${BUILDROOT_RELEASE:=2024.02.9}"
 BUILDROOT_DIR="${BUILDROOT_DIR:=/workspaces/buildroot}"
 
-su $USERNAME
 cd /tmp/
 wget "https://buildroot.org/downloads/buildroot-$BUILDROOT_RELEASE.tar.gz"
 tar -xf "buildroot-$BUILDROOT_RELEASE.tar.gz"
-mkdir -p /workspaces
 mv "buildroot-$BUILDROOT_RELEASE" $BUILDROOT_DIR
 rm -rf "buildroot-$BUILDROOT_RELEASE.tar.gz"
