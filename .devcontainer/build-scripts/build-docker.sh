121b2a0ea4708d1208ca93bb9b5c69888cdf5f22
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-12-25 02:38:47 -0500

    Update Docker image build script

--- .devcontainer/build-scripts/build-docker.sh
+++ .devcontainer/build-scripts/build-docker.sh
@@ -3,33 +3,9 @@ set -e
 
 # Configurable
 SWIFT_BUILDROOT="${SWIFT_BUILDROOT:=$(pwd)}"
-DEFCONFIG="${DEFCONFIG:=swift_arm64_defconfig}"
 
 # Build and push base docker image
 DOCKER_FILE=$SWIFT_BUILDROOT/.devcontainer/Dockerfile
+cd $SWIFT_BUILDROOT
 docker build -t colemancda/buildroot-swift --file $DOCKER_FILE .devcontainer
-
-# Create arch-specific Dockerfile
-DOCKER_FILE_ARCH=$DOCKER_FILE-$DEFCONFIG
-rm -rf $DOCKER_FILE_ARCH
-echo "FROM colemancda/buildroot-swift" >> $DOCKER_FILE_ARCH
-echo "ENV DEFCONFIG=${DEFCONFIG}" >> $DOCKER_FILE_ARCH
-echo "ENV SWIFT_BUILDROOT=/workspaces/buildroot-swift" >> $DOCKER_FILE_ARCH
-echo "ENV BUILDROOT_RELEASE=2024.02.9" >> $DOCKER_FILE_ARCH
-echo "ENV BUILDROOT_DIR=/workspaces/buildroot" >> $DOCKER_FILE_ARCH
-echo "COPY . /workspaces/buildroot-swift/" >> $DOCKER_FILE_ARCH
-echo "COPY .devcontainer/build-scripts/configure.sh /tmp/build-scripts/" >> $DOCKER_FILE_ARCH
-echo "RUN /bin/bash /tmp/build-scripts/configure.sh" >> $DOCKER_FILE_ARCH
-echo "COPY .devcontainer/build-scripts/fetch-sources.sh /tmp/build-scripts/" >> $DOCKER_FILE_ARCH
-echo "RUN /bin/bash /tmp/build-scripts/fetch-sources.sh" >> $DOCKER_FILE_ARCH
-echo "COPY .devcontainer/build-scripts/build-host-swift.sh /tmp/build-scripts/" >> $DOCKER_FILE_ARCH
-echo "RUN /bin/bash /tmp/build-scripts/build-host-swift.sh" >> $DOCKER_FILE_ARCH
-echo "COPY .devcontainer/build-scripts/build-toolchain.sh /tmp/build-scripts/" >> $DOCKER_FILE_ARCH
-echo "RUN /bin/bash /tmp/build-scripts/build-toolchain.sh" >> $DOCKER_FILE_ARCH
-echo "COPY .devcontainer/build-scripts/build-base.sh /tmp/build-scripts/" >> $DOCKER_FILE_ARCH
-echo "RUN /bin/bash /tmp/build-scripts/build-base.sh" >> $DOCKER_FILE_ARCH
-
-# Build Docker image
-docker build -t colemancda/buildroot-swift:$DEFCONFIG --file $DOCKER_FILE_ARCH .
 docker push colemancda/buildroot-swift
-docker push colemancda/buildroot-swift:$DEFCONFIG
