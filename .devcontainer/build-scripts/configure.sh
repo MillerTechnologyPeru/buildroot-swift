323ea8a276e0998119f0bbd3956c75a79f99e2df
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-12-25 03:58:06 -0500

    Fix build scripts

--- .devcontainer/build-scripts/configure.sh
+++ .devcontainer/build-scripts/configure.sh
@@ -6,7 +6,6 @@ SWIFT_BUILDROOT="${SWIFT_BUILDROOT:=$(pwd)}"
 source $SWIFT_BUILDROOT/.devcontainer/build-scripts/swift-define
 
 # Build
-mkdir -p $BR2_DL_DIR
 cd $WORKING_DIR
 echo "make $BUILDROOT_OPTIONS $BUILDROOT_DEFCONFIG"
 make $BUILDROOT_OPTIONS $BUILDROOT_DEFCONFIG
