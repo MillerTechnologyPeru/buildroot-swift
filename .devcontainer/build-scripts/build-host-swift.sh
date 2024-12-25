e16b9ef7f69f705e4889d02b3d19dc505e5e66a2
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-12-25 22:33:01 +0000

    Fixed Swift tools symlink

--- .devcontainer/build-scripts/build-host-swift.sh
+++ .devcontainer/build-scripts/build-host-swift.sh
@@ -8,3 +8,19 @@ source $SWIFT_BUILDROOT/.devcontainer/build-scripts/swift-define
 # Build
 cd $WORKING_DIR
 make $BUILDROOT_OPTIONS host-swift-build
+
+# Create symlinks for multiarch
+declare -a arr=("arm64" "armv7" "armv6" "armv5" "x86_64" "i386" "ppc64le" "ppc" "riscv64" "mips64" "mips")
+for i in "${arr[@]}"
+do
+    if [[ "$i" != "$SWIFT_TARGET_ARCH" ]]; then
+        BUILDROOT_OUTPUT_ARCH=$WORKING_DIR/output/$i
+        mkdir -p $BUILDROOT_OUTPUT_ARCH
+        mkdir -p $BUILDROOT_OUTPUT_ARCH/build/
+        cd $BUILDROOT_OUTPUT_ARCH/build
+        if [ ! -d "$BUILDROOT_OUTPUT_ARCH/build/host-swift-6.0.3" ]; then
+            echo "Generating host tools symlink for $i"
+            ln -s ../../$SWIFT_TARGET_ARCH/build/host-swift-6.0.3 ./host-swift-6.0.3
+        fi
+    fi
+done
