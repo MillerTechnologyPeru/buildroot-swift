e12648418dd352137fc588fa9e0566d8f16012ab
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-12-27 14:27:54 +0000

    Fix building Swift test binary

--- .devcontainer/build-scripts/build-test.sh
+++ .devcontainer/build-scripts/build-test.sh
@@ -7,5 +7,7 @@ source $SWIFT_BUILDROOT/.devcontainer/build-scripts/swift-define
 
 # Build
 cd $WORKING_DIR
+echo "BR2_PACKAGE_SWIFT_HELLO=y" >> $BUILDROOT_CONFIG
 make $BUILDROOT_OPTIONS swift-hello-build
+make $BUILDROOT_OPTIONS
 file $BUILDROOT_TARGET/usr/bin/swift-hello
