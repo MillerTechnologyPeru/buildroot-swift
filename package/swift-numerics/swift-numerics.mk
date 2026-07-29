1d66b31328805449223faf22085256f7c22f13d9
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-07-29 07:35:09 -0400

    swift-numerics: install into the platform directory

--- package/swift-numerics/swift-numerics.mk
+++ package/swift-numerics/swift-numerics.mk
@@ -45,9 +45,9 @@ endef
 
 # See swift-system.mk for why we hand-copy instead of `ninja install`.
 define SWIFT_NUMERICS_INSTALL_STAGING_CMDS
-	find $(SWIFT_NUMERICS_BUILDDIR) -name '*.so' -type f -exec cp -f {} $(STAGING_DIR)/usr/lib/swift/linux/ \;
-	find $(SWIFT_NUMERICS_BUILDDIR) -name '*.a' -type f -exec cp -f {} $(STAGING_DIR)/usr/lib/swift/linux/ \;
-	find $(SWIFT_NUMERICS_BUILDDIR) \( -name '*.swiftmodule' -o -name '*.swiftdoc' -o -name '*.swiftsourceinfo' -o -name '*.abi.json' \) -type f -exec cp -f {} $(STAGING_DIR)/usr/lib/swift/linux/$(SWIFT_TARGET_ARCH)/ \;
+	find $(SWIFT_NUMERICS_BUILDDIR) -name '*.so' -type f -exec cp -f {} $(STAGING_DIR)/usr/lib/swift/$(SWIFT_LIB_SUBDIR)/ \;
+	find $(SWIFT_NUMERICS_BUILDDIR) -name '*.a' -type f -exec cp -f {} $(STAGING_DIR)/usr/lib/swift/$(SWIFT_LIB_SUBDIR)/ \;
+	find $(SWIFT_NUMERICS_BUILDDIR) \( -name '*.swiftmodule' -o -name '*.swiftdoc' -o -name '*.swiftsourceinfo' -o -name '*.abi.json' \) -type f -exec cp -f {} $(STAGING_DIR)/usr/lib/swift/$(SWIFT_LIB_SUBDIR)/$(SWIFT_TARGET_ARCH)/ \;
 	# Stage the _NumericsShims clang module (headers + modulemap). Numerics is
 	# built without library evolution, so consumers transitively require every
 	# module it imports.
